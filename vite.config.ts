f3af18c55dd829bbde87d4dfbb1e4bb96e05491b
ragnarsson03 <samirduran1000@gmail.com>
2025-07-19 04:09:46 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://github.com/ragnarsson03/TaskFlow?tab=readme-ov-file#react--typescript--vite',  // <-- reemplaza "tu-repo" con el nombre exacto de tu repo
+  base: 'https://github.com/ragnarsson03/TaskFlow', 
   plugins: [react()],
 });
