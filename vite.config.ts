4141d7a7c0497f44782218a935a64b367bb55441
ragnarsson03 <samirduran1000@gmail.com>
2025-07-19 04:18:00 -0400

    Fix: Ordenamiento

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://github.com/ragnarsson03/TaskFlow', 
+  base: '/TaskFlow/', 
   plugins: [react()],
 });
