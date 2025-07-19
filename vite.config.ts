613ef98ecccd6558e86519a1f65ec09df0e80c57
ragnarsson03 <samirduran1000@gmail.com>
2025-07-19 04:04:51 -0400

    Update: packgage.json y variables y scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/ragnarsson03/TaskFlow?tab=readme-ov-file#react--typescript--vite',  // <-- reemplaza "tu-repo" con el nombre exacto de tu repo
   plugins: [react()],
-})
+});
